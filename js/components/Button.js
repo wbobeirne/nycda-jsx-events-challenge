2217a5d2326b0debe7e8b1ba7075e52544dcc35b
Will O'Beirne <wbobeirne@gmail.com>
2017-07-25 20:29:24 -0400

    Add loading and success logic to button component

--- js/components/Button.js
+++ js/components/Button.js
@@ -2,11 +2,23 @@ const React = require("react");
 
 class Button extends React.Component {
 	render() {
-		const { children } = this.props;
+		const { children, isLoading, isSuccessful } = this.props;
+		let icon;
+		let buttonClass = "Button";
+
+		if (isLoading) {
+			icon = <span className="Button-loader"/>;
+			buttonClass += " is-loading";
+		}
+		else if (isSuccessful) {
+			icon = <span className="Button-checkmark"/>;
+			buttonClass += " is-successful";
+		}
 
 		return (
-			<button className="Button">
-				{children}
+			<button className={buttonClass}>
+				{icon}
+				{isLoading ? "Loading..." : children}
 			</button>
 		);
 	}
